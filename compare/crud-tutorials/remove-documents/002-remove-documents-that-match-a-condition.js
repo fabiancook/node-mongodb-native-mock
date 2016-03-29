420f71cfd6fc63f0067660b766722a06f6ec687b
Fabian Cook <fabian.cook@nownz.co.nz>
2016-03-29 19:58:48 +1300

    Add remove tutorials

---
+++ compare/crud-tutorials/remove-documents/002-remove-documents-that-match-a-condition.js
@@ -0,0 +1,66 @@
+/*
+ Copyright 2016 Fabian Cook
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+const Expect      = require('chai').expect,
+  Q           = require('q'),
+  Setup       = require('../../setup'),
+  Compare     = require('../../../lib/document/match/bson-compare');
+
+// https://docs.mongodb.org/manual/tutorial/remove-documents/#remove-documents-that-match-a-condition
+describe.only('Remove Documents - Remove Documents that Match a Condition', function(){
+
+  function runSpec(connect) {
+
+    var db, collection;
+
+    before(function(){
+      return connect()
+        .then(function(details) {
+          db = details.db;
+          collection = details.collection;
+        })
+    });
+
+    it('should allow inserts', function(){
+      return collection.insert({ "name": "burgers", "type" : "food" });
+    });
+
+    it('should allow inserts', function(){
+      return collection.insert({ "name": "coke", "type" : "drinks" });
+    });
+
+    it('should allow inserts', function(){
+      return collection.insert({ "name": "fries", "type" : "food" });
+    });
+
+    specify('Remove Documents that Match a Condition', function(){
+      return collection.remove({ "type": "food" });
+    });
+
+    it('should have no documents', function(){
+      return collection.find({})
+        .toArray()
+        .then(function(documents) {
+          Expect(documents).to.have.lengthOf(1);
+          Expect(documents[0].name).to.equal("coke");
+        });
+    })
+
+  }
+
+  Setup.setup(runSpec);
+
+});
